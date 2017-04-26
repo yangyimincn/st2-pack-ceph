6f325f09937063672b7d3c995c0d664b55b0635d
杨益民 <yangyimin@yangyimindeMacBook-Pro.local>
2017-04-26 15:51:00 +0800

    init

--- actions/lib/s3_tools.py
+++ actions/lib/s3_tools.py
@@ -2,9 +2,6 @@
 
 from awsauth import S3Auth
 import requests, json, sys
-from st2actions.runners.pythonrunner import Action
-
-
 
 class UidException(Exception):
     '''
@@ -13,7 +10,6 @@ class UidException(Exception):
     def __init__(self, uid):
         err = 'Uid %s is not exist' % uid
         Exception.__init__(self, err)
-
 
 class s3_admin:
 
@@ -133,64 +129,3 @@ class s3_admin:
 
         except Exception as e:
             print(e)
-
-
-def help():
-    help_str = '''
-        usage: ./s3_admin.py [cluster_id] [uid]
-        example: s3_admin.py infra.ceph.c1 test
-    '''
-    print(help_str)
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        help()
-        exit()
-
-    cluster_id = sys.argv[1]
-    uid = sys.argv[2]
-
-    if cluster_id == 'infra.ceph.c1':
-        cluster_endpoint = 'http://10.0.71.72'
-        access_key = 'Y9Z8WC3MXYGU4XV66BD1'
-        secret_key = '5UtrlvLCqKjHycUBokt3sY7ZiCGHbawaNXrtV16h'
-    elif cluster_id == 'infra.ceph.c4':
-        cluster_endpoint = 'http://10.200.51.204'
-        access_key = 'VA612G91IC8191F52UQW'
-        secret_key = 'rB56vABOnH4AGaRVPY4XLUO4RD1Pkg9qpjvQN6cy'
-    elif cluster_id == 'test':
-        cluster_endpoint = 'http://10.12.10.26:7480'
-        access_key = '5CCOFLUS10VJ31E71DOZ'
-        secret_key = 'F8dH35JwaPPrN4YowkQfvDROkAdNn3LD6zcZlJj3'
-    else:
-        print('Cluster not exists!')
-        exit(0)
-
-    cluster = s3_admin(endpoint=cluster_endpoint, access_key=access_key,
-                  secret_key=secret_key)
-
-    # check user exists!
-    cluster.get_user_info(uid=uid)
-
-    print(cluster.get_user_report(uid=uid))
-
-class MyEchoAction(Action):
-    def run(self, cluster_id, uid):
-        if cluster_id == 'infra.ceph.c1':
-            cluster_endpoint = 'http://10.0.71.72'
-            access_key = 'Y9Z8WC3MXYGU4XV66BD1'
-            secret_key = '5UtrlvLCqKjHycUBokt3sY7ZiCGHbawaNXrtV16h'
-        elif cluster_id == 'infra.ceph.c4':
-            cluster_endpoint = 'http://10.200.51.204'
-            access_key = 'VA612G91IC8191F52UQW'
-            secret_key = 'rB56vABOnH4AGaRVPY4XLUO4RD1Pkg9qpjvQN6cy'
-        elif cluster_id == 'test':
-            cluster_endpoint = 'http://10.12.10.26:7480'
-            access_key = '5CCOFLUS10VJ31E71DOZ'
-            secret_key = 'F8dH35JwaPPrN4YowkQfvDROkAdNn3LD6zcZlJj3'
-        else:
-            return(False, 'cluster_id doest not exists')
-        cluster = s3_admin(endpoint=cluster_endpoint, access_key=access_key,
-                  secret_key=secret_key)
-        cluster.get_user_info(uid=uid)
-        return(True, cluster.get_user_report(uid=uid))
