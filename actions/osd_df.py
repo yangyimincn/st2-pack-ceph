c830972e574754af24f5187120b4f68c4c6bde71
杨益民 <yangyimin@yangyimindeMacBook-Pro.local>
2017-05-01 17:49:03 +0800

    添加OSD df action

---
+++ actions/osd_df.py
@@ -0,0 +1,11 @@
+import requests, json, sys, ConfigParser
+from st2actions.runners.pythonrunner import Action
+from lib.ceph_admin_tools import CephAdmin
+
+
+class GetStatusAction(Action):
+    def run(self, cluster_id, osd_id, reweight):
+
+        ceph_admin = CephAdmin(cluster_id)
+
+        return (True, ceph_admin.osd_df())
